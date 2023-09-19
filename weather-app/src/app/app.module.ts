4ca1db341ae55bbdfa2a3a1d43e44889110501c1
NurseNto <nursentombi0@gmail.com>
2023-09-19 09:01:27 +0200

    Getting the weather per city

--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { WeatherComponent } from './component/weather/weather.component';
+import { WeatherService } from './service/weather.service';
 
 @NgModule({
   declarations: [
+    WeatherComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [WeatherService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ weather-app/src/app/component/weather/weather.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from 'src/app/service/weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+
+  city: string = '';
+  weatherData: any;
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit() {
+  }
+
+
+  getWeather() {
+    if (this.city.trim() !== '') {
+      this.weatherService.getWeather(this.city).subscribe(
+        (data: any) => {
+          this.weatherData = data;
+        },
+        (error: any) => {
+          console.error('Error fetching weather data:', error);
+          // Display a user-friendly error message to the user
+          // You can set a property to show an error message in your template.
+        }
+      );
+    }
+  }
+  
+  
+
+}
