177924f0f595197d621d5976e2eae056f44060ec
NurseNto <nursentombi0@gmail.com>
2023-09-20 09:11:23 +0200

    Add city

--- weather-app/src/app/component/addCity/addCity.component.ts
+++ weather-app/src/app/component/addCity/addCity.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations';
 
 @Component({
   selector: 'app-addCity',
   templateUrl: './addCity.component.html',
-  styleUrls: ['./addCity.component.css']
+  styleUrls: ['./addCity.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('300ms ease-in', style({ transform: 'translateX(0%)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'translateX(100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class AddCityComponent implements OnInit {
+  newCity = '';
+  cities: string[] = [];
+  slideState = 'in';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  
+
+  addCity() {
+    if (this.newCity.trim() !== '') {
+      this.cities.push(this.newCity);
+      this.newCity = '';
+    }
+  }
+
+  deleteCity(index: number) {
+    this.cities.splice(index, 1);
+  }
 }
